0a03f3da2d8a74b22406c53c5d45ae1724fd6198
User <User@User-PC>
2022-05-31 15:23:50 +0530

    first commit

---
+++ Day 01/src/Demo.java
@@ -0,0 +1,11 @@
+class Company{
+	String name;
+	public static void main(String[] args)
+	{
+		Company c = new Company();
+		Company c1 = c;
+		c1.name = "C2TC";
+		c = null;
+		System.out.println(c1.name);
+	}
+}
